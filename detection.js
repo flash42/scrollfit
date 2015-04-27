a18cbe28bc150a9cc3322f2b7c09def1f248b336
Adam Peresztegi <adam.peresztegi@prezi.com>
2015-04-27 14:28:54 +0200

    Pairs test.

--- detection.js
+++ detection.js
@@ -4,12 +4,15 @@ function detectSpeed(tD, yD) {
     var i = 0;
     var fast = false;
     var maxSpeed = 0;
+    var avgSpeed = 0;
     for (; i < tD.length; i++) {
-        if (tD[i] != 0 && maxSpeed < Math.abs((yD[i] / tD[i]))) {
-            maxSpeed = Math.abs((yD[i] / tD[i]));    
+        if (tD[i] == 0) continue;
+        
+        var spd = Math.abs((yD[i] / tD[i]));
+        if (maxSpeed < spd) {
+            maxSpeed = spd;    
         }
-        if (tD[i] != 0 && 1.9 < Math.abs((yD[i] / tD[i]))) {
-            var spd = yD[i] / tD[i];
+        if (1.9 < Math.abs((yD[i] / tD[i]))) {
             fast = true;
             break;
         }
